3360ee3dd1427384f996644053a92408b8772a70
pc1 <mootassame@gmail.com>
2022-07-29 10:07:53 -0400

    show the shadow in the portolio images and the icons github and the icons eyes

--- Frontend/src/components/Portfolio/Portfolio.jsx
+++ Frontend/src/components/Portfolio/Portfolio.jsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from "react";
-import portfolio6 from "../../assets/portfolio6.jpg";
+import portfolio6 from "../../assets/estate.png";
 import portfolio1 from "../../assets/portfolio1.png";
 import portfolio2 from "../../assets/portfolio2.png";
 import { client, urlFor } from "../../client";
 import { motion } from "framer-motion";
 
 function Portfolio() {
-  const [portfolio, setPortfolio] = useState([]);
+  const [images, setImage] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [filterWork, setFilterWork] = useState([]);
+
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === "All") {
+        setFilterWork(images);
+      } else {
+        setFilterWork(images.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
   useEffect(() => {
-    const query = '*[_type == "portfolios"]';
+    const query = '*[_type == "portfolio"]';
     client.fetch(query).then(async (res) => {
-      await setPortfolio(res);
+      await setImage(res);
     });
   }, []);
-
+  console.log("Portfolio", images);
   return (
     <div className='app__portfolio' id='portfolio'>
       <div className='portfolio__header'>
@@ -36,27 +54,45 @@ function Portfolio() {
           </ul>
         </div>
         <motion.div
-          whileHover={{ scale: [1, 0.9] }}
-          transition={{ duration: 0.25 }}
+          animate={animateCard}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
           className='images'>
-          <div>
-            <img loading='lazy' src={portfolio6} alt='image of potfolio' />
-          </div>
-          <div className='item2'>
-            <img loading='lazy' src={portfolio6} alt='image of potfolio' />
-          </div>
-          <div>
-            <img loading='lazy' src={portfolio6} alt='image of potfolio' />
-          </div>
-          <div>
-            <img loading='lazy' src={portfolio6} alt='image of potfolio' />
-          </div>
-          <div>
-            <img loading='lazy' src={portfolio6} alt='image of potfolio' />
-          </div>
-          <div>
-            <img loading='lazy' src={portfolio6} alt='image of potfolio' />
-          </div>
+          {images.map((item) => (
+            <div>
+              <img
+                loading='lazy'
+                src={urlFor(item.imgUrl)}
+                alt='image of potfolio'
+              />
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className='app__work-hover app__flex'>
+                <a href={item.projectLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'>
+                    <i className='fa-solid fa-eye'></i>
+                  </motion.div>
+                </a>
+                <a href={item.codeLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'>
+                    <i className='fa-brands fa-github'></i>
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+          ))}
         </motion.div>
         <div className='button'>load More</div>
       </div>
