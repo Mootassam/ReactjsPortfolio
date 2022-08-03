ca51bcb5a10c399b628034e5dca243395c85c6c2
pc1 <mootassame@gmail.com>
2022-08-03 09:14:00 -0400

    add the react.lazy

--- Frontend/src/components/index.jsx
+++ Frontend/src/components/index.jsx
@@ -1,8 +1,10 @@
-import Header from "./Header/Header";
-import About from "./About/About";
-import Portfolio from "./Portfolio/Portfolio";
-import Skills from "./Skills/Skills";
-import Service from "./Service/Service";
-import Contact from "./Contact/Contact";
+import React from "react";
+
+const Header = React.lazy(() => import("./Header/Header"));
+const About = React.lazy(() => import("./About/About"));
+const Portfolio = React.lazy(() => import("./Portfolio/Portfolio"));
+const Skills = React.lazy(() => import("./Skills/Skills"));
+const Service = React.lazy(() => import("./Service/Service"));
+const Contact = React.lazy(() => import("./Contact/Contact"));
 
 export { Header, About, Portfolio, Skills, Service, Contact };
